8044fbb6fc38ec9bf0680ea47d2379f13714cc58
Judy Hanson <judy.l.hanson@gmail.com>
2025-10-03 22:40:14 -0700

    15 - Installed CORS

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 require('dotenv').config()
 const router = require('./routes/index')
 const connectDB = require('./DB/connection')
+const cors = require('cors')
 
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/', router)
 
