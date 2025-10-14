13178ace82f31b35a4a8cd506f11d78096ef8c34
Judy Hanson <judy.l.hanson@gmail.com>
2025-10-13 22:54:26 -0700

    testing github oauth

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,14 @@ router.use('/shows', require('./showsRoute'));
 
 
 // GitHub authentication route
-router.get('/login', passport.authenticate('github'), (req, res) => {});
+// router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/login', (req, res, next) => {
+  console.log("Redirecting to GitHub with client_id:", process.env.GITHUB_CLIENT_ID);
+  console.log("Using callback URL:", process.env.GITHUB_CALLBACK_URL);
+
+  // hand off to passport
+  passport.authenticate('github')(req, res, next);
+});
 
 // GitHub callback (after successful login)
 router.get(
